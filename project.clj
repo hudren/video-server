49000008e9a5ee318d6b4b1fb192af68dfac3af4
Jeff Hudren <hudren@gmail.com>
2014-12-11 08:19:32 -0700

    Manage bower deps in lein

--- project.clj
+++ project.clj
@@ -19,10 +19,21 @@
                  [compojure "1.3.1"]
                  [enlive "1.1.5"]]
 
+  :plugins [[lein-bower "0.5.1"]]
+
+  :bower-dependencies [[polymer "~0.5.1"]
+                       [font-roboto "Polymer/font-roboto#~0.5.1"]
+                       [core-header-panel "Polymer/core-header-panel#~0.5.1"]
+                       [core-toolbar "Polymer/core-toolbar#~0.5.1"]
+                       [paper-button "Polymer/paper-button#~0.5.1"]]
+
+  :bower {:directory "resources/public/components"}
+
   :main video-server.main
   :aot [video-server.main]
   :uberjar-name "videos@home.jar"
 
   :profiles {:dev {:source-paths ["dev"]}}
 
-  :aliases {"docs" ["do" "marg" "-m," "shell" "open" "docs/toc.html"]})
+  :aliases {"build" ["do" "clean," "bower" "install," "uberjar"]
+            "docs" ["do" "marg" "-m," "shell" "open" "docs/toc.html"]})
