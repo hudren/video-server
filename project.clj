579ff92738ba64494367524390d613f522208b82
Jeff Hudren <hudren@gmail.com>
2016-05-06 06:27:18 -0600

    Releasing 0.5.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.hudren.homevideo/video-server "0.5.2-SNAPSHOT"
+(defproject com.hudren.homevideo/video-server "0.5.2"
   :description "Videos@Home server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
