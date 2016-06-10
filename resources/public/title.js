7a02dcaa618f3177d372a05bfbb55ab720c81320
Jeff Hudren <hudren@gmail.com>
2016-06-10 07:36:28 -0600

    Fix for Chrome video controls

--- resources/public/title.js
+++ resources/public/title.js
@@ -1,22 +1,40 @@
+"use strict";
+
+function showControls() {
+  var vid = document.getElementsByTagName('video')[0];
+  if (vid) {
+    vid.setAttribute("controls", "true")
+  }
+}
+
 function showVideo() {
   // Show player
   var block = document.getElementById("player");
   if (block) {
     block.style.visibility = "visible";
   }
+
+  // Chrome needs a delay
+  window.setTimeout(showControls, 500)
+  window.setTimeout(showControls, 1000)
+  window.setTimeout(showControls, 2000)
+  window.setTimeout(showControls, 5000)
 }
 
 function hideVideo() {
-  var vid = document.getElementsByTagName('video')[0];
+  var vid = document.getElementsByTagName("video")[0];
   if (vid) {
-    vid.addEventListener("canplaythrough", showVideo, true);
+    vid.addEventListener("canplay", showVideo);
+
+    // Hide controls
+    // vid.removeAttribute("controls")
   }
 
   // Safari needs help
   window.setTimeout(showVideo, 500);
 }
 
+// Add 'js' class to head element if Javascript is enabled
 (function () {
-  // Add 'js' class to head element if Javascript is enabled
   document.documentElement.className += " js";
 }());
